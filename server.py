8fffce5be6002846b9664b8066ae371cbff6ba47
Bearcat2020 <jbarakat20@kentdenver.org>
2018-09-09 14:20:59 -0600

    started the very very beginning of the server with flask

--- server.py
+++ server.py
@@ -1,2 +1,14 @@
-#this is my server
-from flask import Flask
+#these are the imports
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+#this is the decorator. it shows what to return when i go to the website
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
